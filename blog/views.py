534ea60e8ee86b7a7b36c7185453b5f96c2bb047
Ryu <kinzilal@khcu.ac.kr>
2025-05-26 18:02:31 +0900

    페이지별 HTML 템플릿 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 # blog/views.py
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("블로그 첫 화면입니다!")
+    return render(request, 'blog/index.html')  # 메인 버튼 페이지
+
+def page1(request):
+    return render(request, 'blog/page1.html')
+
+def page2(request):
+    return render(request, 'blog/page2.html')
+
+def page3(request):
+    return render(request, 'blog/page3.html')
+
+def page4(request):
+    return render(request, 'blog/page4.html')
