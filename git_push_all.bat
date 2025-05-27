62e9237c9992b8cd878a9261fa491ced64d81157
Ryu <kinzilal@khcu.ac.kr>
2025-05-27 09:20:27 +0900

    ?꾩껜 ?낅뜲?댄듃

--- git_push_all.bat
+++ git_push_all.bat
@@ -1,14 +1,9 @@
 @echo off
-chcp 65001 > nul
-
 cd /d C:\Users\XERIX_RESEARCH\mysite
 call venv\Scripts\activate
 
 git add .
-
-set /p commitmsg=Enter commit message: 
-git commit -m "%commitmsg%"
-
+git commit -m "전체 업데이트"
 git push origin main
 
 pause
